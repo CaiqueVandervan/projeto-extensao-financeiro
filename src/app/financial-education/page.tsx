9047680787a192dbc5922f98bb9e8de7f361d386
Caique481 <caique.araujo@gmaster.com.br>
2026-07-16 11:07:04 -0300

    feat: add BreadCrumbs component to page

--- src/app/financial-education/page.tsx
+++ src/app/financial-education/page.tsx
@@ -1,13 +1,24 @@
 "use client"
 
 import TipsModals from "../../components/TipsModals"
+import BreadCrumbs from "@/components/BreadCrumbs"
 
 const FinancialEducation = () => {
+
+    const breadCrumbsItems = [
+        { label: "Apresentação", url: "/" },
+        { label: "Dicas Financeiras", url: "/" }
+    ]
+
     return (
         <main className="relative h-screen flex items-center justify-center">
             <div className="absolute inset-0 bg-[url(/fundo2.png)] z-0 bg-center bg-cover" />
-            <header className="absolute top-16 text-[#f8fafc] font-extrabold text-4xl z-10">
+            <header className="absolute top-16 text-[#f8fafc] font-extrabold text-4xl w-full flex items-center justify-center">
+
+                <BreadCrumbs items={breadCrumbsItems} />
+
                 Dicas Financeiras
+
             </header>
             <TipsModals />
         </main>
