511176ea4043f378f3a1b02c520a444a7649de0c
Caique481 <caique.araujo@gmaster.com.br>
2026-07-16 11:06:14 -0300

    feat: create reusable BreadCrumbs component

---
+++ src/components/BreadCrumbs.tsx
@@ -0,0 +1,45 @@
+"use client"
+
+import { useRouter } from "next/navigation"
+
+interface BreadCrumbsItem {
+    label: string
+    url: string
+}
+
+interface BreadCrumbsArray {
+    items: BreadCrumbsItem[]
+}
+
+export default function BreadCrumbs({ items }: BreadCrumbsArray) {
+
+    const router = useRouter()
+
+    return (
+        <div className="absolute left-16 flex gap-3 text-[16px]">
+            {items.map((item, index) => {
+
+                const lastItem = index === items.length - 1
+
+                return (
+                    <div key={item.label} className="flex items-center">
+                        {lastItem ? (
+                            <span>{item.label}</span>
+                        ) : (
+                            <div className="flex items-center gap-3">
+                                <button className="hover:scale-103 duration-100 cursor-pointer"
+                                    onClick={() => router.push(item.url)}>
+                                    {item.label}
+                                </button>
+                                <span className="text-[32px]">{">"}</span>
+                            </div>
+
+                        )}
+
+
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
